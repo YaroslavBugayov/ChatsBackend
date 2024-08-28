04a41cb00ec90271b2e93bb2b438fe2a7d00dd5b
YaroslavBugayov <bugayov576@gmail.com>
2024-08-28 07:08:19 +0300

    * socket cors

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,23 @@
 import { Server as SocketIOServer, Socket } from 'socket.io';
 import { app } from './app';
 import http from "http";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const server: http.Server = http.createServer(app);
 
-const io: SocketIOServer = new SocketIOServer(server);
+const io: SocketIOServer = new SocketIOServer(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL,
+        credentials: true
+    }
+});
 
 io.on('connection', (socket: Socket) => {
     console.log(`${socket.id} connected`);
+
+    // socket.emit('message', JSON.stringify({ users: ['Dave', 'Emma', 'John', 'Peter'] }));
 
     socket.on('disconnect', () => {
         console.log(`${socket.id} disconnected`);
